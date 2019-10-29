5131d1ec53ad3ad775fc6233b78e301def109405
Ruslan <rusik.bespalov54@gmail.com>
2019-10-29 17:00:26 +0200

    url2

--- index.php
+++ index.php
@@ -2,6 +2,25 @@
 // Version
 define('VERSION', '3.0.2.0');
 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+}
+
+//echo json_encode(array("test" => "test_df111"));
+
+//exit();
+
 // Configuration
 if (is_file('config.php')) {
 	require_once('config.php');
