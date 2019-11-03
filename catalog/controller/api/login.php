b785c224fa53d9eb4258d65de8adeb275df96fbd
tartakovskyi <vvt2001@ukr.net>
2019-11-03 22:39:33 +0200

    403

--- catalog/controller/api/login.php
+++ catalog/controller/api/login.php
@@ -7,23 +7,24 @@ class ControllerApiLogin extends Controller {
 
 		$this->load->model('account/api');
 
-		// Login with API Key
-		$api_info = $this->model_account_api->login($this->request->post['username'], $this->request->post['key']);
+		if (isset($_POST['username']) && isset($_POST['key'])) {
 
-		if ($api_info['username'] && $api_info['key']) {
+		// Login with API Key
+			$api_info = $this->model_account_api->login($this->request->post['username'], $this->request->post['key']);
+
 			// Check if IP is allowed
 			$ip_data = array();
-	
+
 			$results = $this->model_account_api->getApiIps($api_info['api_id']);
-	
+
 			foreach ($results as $result) {
 				$ip_data[] = trim($result['ip']);
 			}
-	
+
 			if (!in_array($this->request->server['REMOTE_ADDR'], $ip_data)) {
 				$json['error']['ip'] = sprintf($this->language->get('error_ip'), $this->request->server['REMOTE_ADDR']);
 			}				
-				
+
 			if (!$json) {
 				$json['success'] = $this->language->get('text_success');
 				
@@ -39,12 +40,24 @@ class ControllerApiLogin extends Controller {
 			} else {
 				$json['error']['key'] = $this->language->get('error_key');
 			}
+
 		} else {
-			$json['error']['key'] = 'Отсутствует логин или пароль';
-			$this->response->addHeader('HTTP/1.1 403 Forbidden');
+
+			sendError(403, 'Не указан логин или пароль');
+
+
+			// http_response_code (403);
+			// $json[] = 'Не указан логин или пароль';
+
 		}
-		
+
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
+
+		function sendError ($code, $errTxt) {
+			http_response_code ($code);
+			$json[] = $errTxt;
+		}
+
 	}
 }
